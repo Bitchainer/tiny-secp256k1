5fbd8c65d64b81bf037e8bf1cf861feee070b970
CW-75 <cw75dev@gmail.com>
2025-11-17 01:47:30 -0400

    fixed repo

---
+++ test/tiny_secp256k1_test.dart
@@ -0,0 +1,18 @@
+// ToDO implement some tests
+
+// import 'package:tiny_secp256k1/tiny_secp256k1.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   group('A group of tests', () {
+//     final awesome = Awesome();
+
+//     setUp(() {
+//       // Additional setup goes here.
+//     });
+
+//     test('First Test', () {
+//       expect(awesome.isAwesome, isTrue);
+//     });
+//   });
+// }
